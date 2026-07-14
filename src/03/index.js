bd8eb7a0b0c1207229ce1584169af36ed745d486
Katia Mesquita <katiamesquita@hotmail.com>
2026-07-13 21:12:03 -0300

    fazendo ex 3

---
+++ src/03/index.js
@@ -0,0 +1,20 @@
+
+
+const meuDinheiro = 80;
+const valorTenis = 130;
+
+const valorDesconto = valorTenis - meuDinheiro;
+
+console.log("O desconto será de R$ ", valorDesconto);
+
+const porcentagem = valorDesconto * 100 / valorTenis ;
+
+console.log ("A porcentagem do desconto será de " + "%", porcentagem.toFixed(2))
+
+
+
+
+
+
+
+
