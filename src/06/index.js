9c848ea9dd10a1d8541f1adee5706010d73129b5
Katia Mesquita <katiamesquita@hotmail.com>
2026-07-14 14:57:18 -0300

    finalizado

---
+++ src/06/index.js
@@ -0,0 +1,14 @@
+
+const n = 6;
+
+const s = (n - 2) * 180;
+console.log (s);
+
+const a = s / n;
+console.log (a);
+
+const somaAngulo = a * 6;
+console.log (somaAngulo)
+
+
+
---
+++ src/07/index.js
@@ -0,0 +1,12 @@
+
+const a = 25;
+const b = 27;
+const c = 7;
+
+const delta = (b)**2 - 4 * a * c;
+console.log (delta.toFixed(2));
+
+
+
+
+
